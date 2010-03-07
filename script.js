c781a9ffa61010d7ed4d9042b38e404d4c9672b8
lucideer <lucideer@localhost>
2010-03-07 16:07:33 +0000

    fun with hgignore

--- script.js
+++ script.js
@@ -23,4 +23,3 @@ function index(e){
   }
   else{ output.page(response,'<div id="faketoolbar"><a class="button" title="WIDGETISE" href="opera:/button/Go%20to%20page,%20%22'+'javascript:(function(obj,s){s.w=window.screen.width;s.h=window.screen.height;obj.name=prompt(\'Widgetname:\');obj.title=document.title;obj.screen=s;obj.url=location.href;obj.icon=location.protocol+\'//\'+document.domain+\'/favicon.ico\';location.href=\'http://'+opera.io.webserver.hostName+opera.io.webserver.currentServicePath+'?w=\'+encodeURIComponent(JSON.stringify(obj));})({},{});'+'%22,1,,%22Panel%20Widgets%22">WIDGETISE</a></div>'); }
 }
-
