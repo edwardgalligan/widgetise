b25af971ef22724b27fd4233e1a796ce622b17ac
lucideer <lucideer@localhost>
2010-08-23 18:58:15 +0100

    Minor formatting cleanups

--- output.js
+++ output.js
@@ -1,30 +1,36 @@
-var output={
-  file:function(file,response,mime,c){
-    if(file.exists){
-      response.setResponseHeader('content-type',mime);
+var output=
+{
+  file:function(file,response,mime,c)
+  {
+    if(file.exists)
+    {
+      response.setResponseHeader('content-type', mime);
       response.writeFile(file);
     }
     if(c){ response.close(); }
   },
 
-  page:function(r,content){
-    this.file(appDir.resolve('index/header.xhtml'),r,'application/xhtml+xml',0);
+  page:function(r,content)
+  {
+    this.file(appDir.resolve('index/header.xhtml'), r, 'application/xhtml+xml',0);
     r.write(content);
     r.write('</body></html>');
     r.close();
   },
 
-  css:function(e){ output.file(appDir.resolve('index/styles.css'),e.connection.response,'text/css',1); },
+  css:function(e){ output.file(appDir.resolve('index/styles.css'), e.connection.response,'text/css',1); },
 
-  widget:function(r,filename){
+  widget:function(r,filename)
+  {
     var widg=dir.resolve(filename);
-    if(widg.exists){
-      r.setResponseHeader('content-disposition','attachment;filename='+filename);
-      output.file(widg,r,'application/x-opera-widgets',1);
+    if(widg.exists)
+    {
+      r.setResponseHeader('content-disposition', 'attachment;filename='+filename);
+      output.file(widg, r, 'application/x-opera-widgets', 1);
     }
     r.close();
     
     /* give the user 2 minutes to click through the installation dialogues before deleting the widget installer */
-    window.setTimeout(function(){dir.deleteFile(widg);},120000);
+    window.setTimeout(function(){ dir.deleteFile(widg); }, 120000);
   }
 };
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var appDir=opera.io.filesystem.mountSystemDirectory('application'), dir=opera.io.filesystem.mountSystemDirectory('storage');
 
-window.onload=function(){
+window.onload=function()
+{
   var webserver=opera.io.webserver;
   if(webserver){ webserver.addEventListener('_index',index,0); webserver.addEventListener('css',output.css,0); }
 };
@@ -11,24 +12,29 @@ function index(e){
   
   if(!e.connection.isLocal){ response.write('Sorry, this is a local application.'); response.close(); return; }
 
-  if(request.queryItems.w){
+  if(request.queryItems.w)
+  {
     output.widget( response, widgetise.create( JSON.parse(decodeURIComponent(request.queryItems.w[0])) ) );
-  }else{
-    output.page(
+  }else
+  {
+    output.page
+    (
       response,
-      '<div id="faketoolbar">'+"\n"+
-        '<a class="button" title="WIDGETISE" href="opera:/button/Go%20to%20page,%20%22'+
-          'javascript:'+
-            '(function(obj,s){'+
-              "obj.name=prompt('Widgetname:');"+
-              'obj.title=document.title;'+
-              'obj.screen={w:window.screen.width,h:window.screen.height};'+
-              'obj.url=location.href;'+
-              "obj.icon=location.protocol+'//'+document.domain+'/favicon.ico';"+
-              "location.href='http://"+opera.io.webserver.hostName+opera.io.webserver.currentServicePath+"?w='+encodeURIComponent(JSON.stringify(obj));"+
-            '})({},{});'+
-        '%22,1,,%22Panel%20Widgets%22">WIDGETISE</a>'+"\n"+
-      '</div>'+"\n"
+      [
+        '<div id="faketoolbar">',
+          '<a class="button" title="WIDGETISE" href="opera:/button/Go%20to%20page,%20%22',
+          'javascript:',
+            '(function(obj,s){',
+              "obj.name=prompt('Widgetname:');",
+              'obj.title=document.title;',
+              'obj.screen={w:window.screen.width,h:window.screen.height};',
+              'obj.url=location.href;',
+              "obj.icon=location.protocol+'//'+document.domain+'/favicon.ico';",
+              "location.href='http://"+opera.io.webserver.hostName+opera.io.webserver.currentServicePath+"?w='+encodeURIComponent(JSON.stringify(obj));",
+            '})({},{});',
+          '%22,1,,%22Panel%20Widgets%22">WIDGETISE</a>',
+        '</div>'
+      ].join('')
     );
   }
 }
